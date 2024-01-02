e0fbb47f5aaf27678e73faed09c29b7b1e9beca4
saurabhrawat-13 <155474450+saurabhrawat-13@users.noreply.github.com>
2024-01-02 19:25:15 +0000

    done

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "saurabhrawat1313@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+//accountId = 2 //
+console.log(accountId);
+accountEmail = "saurabhrawat776@gmail.com"
+accountPassword = "987654"
+accountCity = "kashipur"
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
