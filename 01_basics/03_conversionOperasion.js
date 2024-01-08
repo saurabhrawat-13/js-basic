ef306bc47f3b5cab09624d97c1342333da9248a8
saurabhrawat-13 <155474450+saurabhrawat-13@users.noreply.github.com>
2024-01-08 19:10:14 +0000

    done

---
+++ 01_basics/03_conversionOperasion.js
@@ -0,0 +1,60 @@
+let score = false;
+
+console.log(typeof score);
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber);
+
+console.log(valueinnumber);
+
+// "33" = 33
+// "33abc" = NaN
+// "True" = 1,"false" = 0,
+let IsLogedIn = 1
+ 
+let booleanIslogedIn = "true";
+console.log(booleanIslogedIn);
+
+// 1 = true; 0 = false;
+//"" = false;
+//"saurabh" = true
+
+let someno = 33
+
+let stringNumber = String(someno)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+//***************OPERATION********************* */
+
+let value = 33
+let negvalue = -value;
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(3%2);
+
+
+
+let a = "saurabh";
+let b = " amit"
+
+let c = a+b;
+console.log(c);
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log(1 + "2" +3);
+console.log(1 + 2 +"3");
+
+console.log((3 + 3) * 2); // use brackets to write professionally 
+
+let game = 100
+game++
+console.log(game);
