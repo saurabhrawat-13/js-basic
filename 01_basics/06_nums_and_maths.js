a7bdcf03ce11ede15ba76d87df67d8b82d068b2e
saurabhrawat-13 <155474450+saurabhrawat-13@users.noreply.github.com>
2024-02-09 11:49:56 +0000

    done

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,24 @@
+const score = 400 
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));// to fixed for  precision value
+
+const otherNumber = 123.53445
+
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000000
+console.log(hundreds.toLocaleString());
+
+
+
+
+//  ++++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++++++++++++
+
+
+
+
