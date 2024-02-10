98116f3bcdfd35405e4be076d617c10be3a8c6dc
saurabhrawat-13 <155474450+saurabhrawat-13@users.noreply.github.com>
2024-02-10 08:04:26 +0000

    done

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -17,7 +17,24 @@ console.log(hundreds.toLocaleString());
 
 
 
-//  ++++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++++++++++++
+//  ++++++++++++++++++++++++++ MATHS +++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(3.9));
+console.log(Math.min(3,9,1));
+console.log(Math.max(3,9,1));
+console.log(Math.random());
+console.log(Math.random()*9);
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min );
+
+
 
 
 
