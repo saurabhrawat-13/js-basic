b68f55fd9eb2b8de99690e5caa7c6e6189ec7ecf
saurabhrawat-13 <155474450+saurabhrawat-13@users.noreply.github.com>
2024-02-10 14:56:18 +0000

    done

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -38,4 +38,3 @@ console.log(Math.floor(Math.random() * (max - min + 1)) + min );
 
 
 
-
---
+++ 01_basics/DateandTime.js
@@ -0,0 +1,2 @@
+const x = new Date().toLocaleTimeString();// use . just after Date() to learn more
+console.log(x);
