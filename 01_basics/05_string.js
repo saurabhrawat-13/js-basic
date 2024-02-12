3b301508617ce7b8f301a17505d05df90064a052
saurabhrawat-13 <155474450+saurabhrawat-13@users.noreply.github.com>
2024-02-12 14:23:30 +0000

    done

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -6,13 +6,12 @@ const repoCount = 50
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('saurabhrawat')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
 
-// console.log(gameName[0]);
-// console.log(gameName.__proto__);
 
-
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('r'));
 
