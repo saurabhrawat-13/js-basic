88fcd1f643e08a066c2ab1bdc2c341a03d2af20e
saurabhrawat-13 <155474450+saurabhrawat-13@users.noreply.github.com>
2024-02-13 11:33:40 +0000

    Done

---
+++ 01_basics/09_Object.js
@@ -0,0 +1,26 @@
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Saurabh",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser.email)
+console.log(JsUser["email"]) 
+console.log(JsUser["full name"]) 
+console.log(JsUser[mySym])
+
+
+
