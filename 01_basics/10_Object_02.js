249494a3727f3cd2d9e26b74d0ffbd91c20b97a2
saurabhrawat-13 <155474450+saurabhrawat-13@users.noreply.github.com>
2024-02-14 14:01:23 +0000

    Done

---
+++ 01_basics/10_Object_02.js
@@ -0,0 +1,19 @@
+// const tinderUser = new Object()
+
+const tinderUser = {}
+tinderUser.id = "123@33"
+tinderUser.name = "saurabh"
+tinderUser.isloggedIn = false
+
+// console.log(tinderUser);
+
+
+const regularUser =  {
+    email: "saurabh@gmail.com",
+    fullname: {
+        userfullname: {
+            
+        }
+
+    }
+}
