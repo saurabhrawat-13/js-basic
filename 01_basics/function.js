953c24e1ff03a4ab592b73408a58868c19202b5c
saurabhrawat-13 <155474450+saurabhrawat-13@users.noreply.github.com>
2024-02-16 12:35:10 +0000

    DONE

---
+++ 01_basics/function.js
@@ -0,0 +1,28 @@
+// // function hello(){};//funciton decleration
+// function hello()
+// {
+//     console.log("hello")
+// }
+// // hello()
+//  const hello2=hello
+ 
+// //  ()=>{
+// //      console.log('hello2')
+// //  }
+//  hello2()
+//  x="saurabh"
+//  const hello3=x=>console.log(x)
+ 
+// //  hello3(x)
+
+//  const myTimeout = setInterval(hello2, 2000);
+ 
+// function add(a,b){
+// console.log(a+b)
+// }
+
+const  add=(a,b)=>{
+    console.log(a+b)
+    }
+    add(1,2)
+add(3,4)
---
+++ 02_basics/01_function.js
@@ -0,0 +1,27 @@
+
+function sayMyNam(){
+    
+console.log("s");
+console.log("a");
+console.log("u");
+console.log("r");
+console.log("a");
+console.log("b");
+console.log("h");
+}
+sayMyNam()
+
+// function addTwoNumbers(number1,number2){
+//     console.log(number1 + number2);
+    
+// }
+
+
+function addTwoNumbers(number1,number2){
+    // let result = number1 + number2  
+    // return result
+    return number1 + number2
+}
+const result = addTwoNumbers(4,5)
+// console.log("Result",result);
+
