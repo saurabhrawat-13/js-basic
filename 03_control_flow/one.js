69f5eb3aa82e0b203505a6387ffeada5716863ad
saurabhrawat-13 <155474450+saurabhrawat-13@users.noreply.github.com>
2024-04-15 18:20:41 +0000

    done

--- 03_control_flow/one.js
+++ 03_control_flow/one.js
@@ -1,10 +1,31 @@
 //if
 const isUserloggedIn = true
-const temperature = 41
+const temperature = 55
 //if (2 === "2") {
-    if (temperature < 50) {
-        
-    }
-console.log("less than 50");
+//     if (temperature < 50) {
+//         console.log("less than 50");
+//     }else{
+//         console.log("temperature is greater than 50");
+//     }
 
+// console.log("Execute");
 // <,>,<=,>=,==,!=,===,!==
+ // const score= 200
+// if (score > 100) {
+//     var power ="fly"
+    
+// }
+// console.log(`User power: ${power}`);
+
+
+const balance = 1000
+
+//if (balance > 500) console.log("test"),console.log("test2"); should not be done in a code
+
+if (balance > 500){
+    console.log("less than 500");
+}else if (balance < 750);{
+    console.log("less than 750");
+
+
+}
