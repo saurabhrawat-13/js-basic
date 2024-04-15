1b47a8713204fc578b069a248efd710cfb45abe6
saurabhrawat-13 <155474450+saurabhrawat-13@users.noreply.github.com>
2024-04-15 19:58:19 +0000

    done

--- 03_control_flow/one.js
+++ 03_control_flow/one.js
@@ -18,14 +18,27 @@ const temperature = 55
 // console.log(`User power: ${power}`);
 
 
-const balance = 1000
+//const balance = 1000
 
 //if (balance > 500) console.log("test"),console.log("test2"); should not be done in a code
 
-if (balance > 500){
-    console.log("less than 500");
-}else if (balance < 750);{
-    console.log("less than 750");
+// if (balance < 500){
+//     console.log("less than 500");
+// } else if (balance < 750){
+//     console.log("less than 750");
+// } else if (balance < 900){
+//     console.log("less than 750");
+// } else {
+//     console.log("less than 1200");
 
+// }
 
+const userloggedIn = true
+const debitcard = true
+
+if (userloggedIn && debitcard && 2==3) {
+    console.log("allow to buy course");
+    
 }
+
+
