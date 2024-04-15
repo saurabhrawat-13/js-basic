4215b451233ee5e7f43793755b48477ca819d9c5
saurabhrawat-13 <155474450+saurabhrawat-13@users.noreply.github.com>
2024-04-15 16:15:35 +0000

    done

---
+++ 03_control_flow/one.js
@@ -0,0 +1,10 @@
+//if
+const isUserloggedIn = true
+const temperature = 41
+//if (2 === "2") {
+    if (temperature < 50) {
+        
+    }
+console.log("less than 50");
+
+// <,>,<=,>=,==,!=,===,!==
