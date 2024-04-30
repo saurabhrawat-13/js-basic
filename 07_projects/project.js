951cfa993d4568bb66cfc7a65b1559caae8c6c91
saurabhrawat-13 <155474450+saurabhrawat-13@users.noreply.github.com>
2024-04-30 12:23:12 +0000

    DONE

---
+++ 07_projects/project.js
@@ -0,0 +1,13 @@
+const buttons = document.querySelectorAll('.btn');
+console.log(buttons);
+// const body = document.querySelector("body")
+
+
+// buttons.forEach(function(btn){
+//     console.log(btn);
+//     btn.addEventListener('click',function(e){
+//         console.log(e);
+//         console.log(e.target);
+
+    // });
+// });
