46e12e2a55d66acee80a1e09d65b005276270ad2
saurabhrawat-13 <155474450+saurabhrawat-13@users.noreply.github.com>
2024-04-30 15:05:15 +0000

    done

--- 07_projects/project.js
+++ 07_projects/project.js
@@ -1,13 +1,26 @@
 const buttons = document.querySelectorAll('.btn');
-console.log(buttons);
-// const body = document.querySelector("body")
+const body = document.querySelector("body")
 
 
-// buttons.forEach(function(btn){
-//     console.log(btn);
-//     btn.addEventListener('click',function(e){
-//         console.log(e);
-//         console.log(e.target);
-
-    // });
-// });
+buttons.forEach(function(btn){
+     console.log(btn);
+     btn.addEventListener('click',function(e){
+     console.log(e);
+     console.log(e.target);
+     if (e.target.id === 'white' ) {
+        body.style.backgroundColor = e.target.id;    
+     }
+     if (e.target.id === 'orange' ) {
+        body.style.backgroundColor = e.target.id;    
+     }
+     if (e.target.id === 'aqua' ) {
+        body.style.backgroundColor = e.target.id;    
+     }
+     if (e.target.id === 'violet' ) {
+        body.style.backgroundColor = e.target.id;    
+     }
+     if (e.target.id === 'grey' ) {
+        body.style.backgroundColor = e.target.id;    
+     }
+ });
+});
