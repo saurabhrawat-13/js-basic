da626cd8d637b324f0d9bde240163ac45eec4a05
saurabhrawat-13 <155474450+saurabhrawat-13@users.noreply.github.com>
2024-05-02 07:42:42 +0000

    done

---
+++ 08_02_Project/app.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+    const button = (document.querySelector('#btn'));
+    button.addEventListener('click', calculatebmi)
+        console.log('clicked');
+    }
+
+    function calculatebmi(){
+        const height = document.querySelector('#height')
+        const weight = document.querySelector('#weight')
+    
+    }
