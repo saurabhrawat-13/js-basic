bca07714cb1dc4892ac9a771c090415749507b1a
saurabhrawat-13 <155474450+saurabhrawat-13@users.noreply.github.com>
2024-05-12 04:53:22 +0000

    done

--- 08_02_Project/app.js
+++ 08_02_Project/app.js
@@ -1,11 +1,22 @@
 window.onload = () => {
     const button = (document.querySelector('#btn'));
-    button.addEventListener('click', calculatebmi)
-        console.log('clicked');
+    button.addEventListener('click',calculatebmi)
     }
 
     function calculatebmi(){
-        const height = document.querySelector('#height')
-        const weight = document.querySelector('#weight')
+        const height = document.querySelector('#height').value;
+        const weight = document.querySelector('#weight').value;
+        const result = document.querySelector('#result');
+
+        if (!height || isNaN(height) || height < 0) {
+            result.innerText = "Please provide valid Height";
+            result;
+        }else if (!weight || isNaN(weight) || weight < 0) {
+            result.innerText = "Please provide valid Weight"
+                        
+        }
+
+        
+
     
     }
